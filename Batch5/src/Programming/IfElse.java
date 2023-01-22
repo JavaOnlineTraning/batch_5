3006d0d0b3d9d6d9bf59dbf0b6180fa3211d6d07
PRITI S <PRITI S@DESKTOP-FCMLE2V>
2023-01-22 22:00:42 +0530

    Changes

--- Batch5/src/Programming/IfElse.java
+++ Batch5/src/Programming/IfElse.java
@@ -2,8 +2,8 @@ package Programming;
 
 public class IfElse {
 public static void main(String[] args) {
-	int a=10;
-	if(a<20)
+	int a=20;
+	if(a<30)
 	{System.out.println("a is smaller");
 	}
 	else
