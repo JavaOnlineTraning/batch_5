65beac42e2ca96a5bc37fd80a58de8c59636a1e7
MyLapi <MyLapi@DESKTOP-FF5R8SR>
2023-01-28 23:09:32 +0530

    Swapping without Third variable

--- Batch5/src/com/basic/Swapping.java
+++ Batch5/src/com/basic/Swapping.java
@@ -1,9 +1,21 @@
 package com.basic;
 
+import java.util.Scanner;
+
 public class Swapping {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a, b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Plz Enter The Number");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println("Before Swapping Number :" + a + " " + b);
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println("After Swapping Number :" + a + " " + b);
+		System.out.println();
 
 	}
 
