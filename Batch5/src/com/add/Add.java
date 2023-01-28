f7adbba410b5b95fda221ae26c1a954158e81291
HP <HP@DESKTOP-UFADM6T>
2023-01-28 22:47:19 +0530

    added variable

--- Batch5/src/com/add/Add.java
+++ Batch5/src/com/add/Add.java
@@ -2,8 +2,8 @@ package com.add;
 
 public class Add {
 	public static void main(String[] args) {
-		int add,a=5,b=89;
-		add=a+b;
+		int add,a=5,b=89,c=5;
+		add=a+b+c;
 		System.out.println("the sum is"+add);
 	}
 	
