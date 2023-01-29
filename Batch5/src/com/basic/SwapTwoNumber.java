695b05e7e1d30193f3223be1404431584baf982e
LENOVO <LENOVO@LAPTOP-7TGQPK2J>
2023-01-29 14:29:21 +0530

    SwapTwoNumber

---
+++ Batch5/src/com/basic/SwapTwoNumber.java
@@ -0,0 +1,18 @@
+package com.basic;
+import java.util.Scanner;
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the value x and y");
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		System.out.println("Before swapping number: " + x + " " +y);
+		x = x+y;
+		y = x-y;
+		x = x-y;
+		System.out.println("After swapping number: " + x + " " +y);
+
+	}
+
+}
