529df080d55d177ecd07ae92ca1a3ba0a8a7edbe
LENOVO <LENOVO@LAPTOP-7TGQPK2J>
2023-01-29 14:22:57 +0530

    SwapNumber

---
+++ Batch5/src/com/basic/SwappingNum.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class SwappingNum {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c;
+		//Swapping using third variable
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+
+	}
+
+}
