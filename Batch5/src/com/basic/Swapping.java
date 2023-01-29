eeaefca7558c7333265262236d21d75793a1744a
PRITI S <PRITI S@DESKTOP-FCMLE2V>
2023-01-29 21:53:01 +0530

    without third variable

--- Batch5/src/com/basic/Swapping.java
+++ Batch5/src/com/basic/Swapping.java
@@ -1,10 +1,23 @@
 package com.basic;
 
+import java.util.Scanner;
+
 public class Swapping {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			int x,y,t;// x & y swapping
+			Scanner sc= new Scanner(System.in);
+					System.out.println("Enter the value of x & y");
+			x=sc.nextInt();
+			y=sc.nextInt();
+			System.out.println("Before Swapping No: "+x +" "+y);
+			t=x;
+			x=y;
+			y=t;
+			System.out.println("After Swapping No: "+x +" "+y);
+			System.out.println();
+		}
+		}
 
-	}
 
-}
