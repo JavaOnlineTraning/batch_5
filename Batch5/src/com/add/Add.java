884bf4e361956228998266edde0fd7765d659b0d
anike <anike@LAPTOP-6FH2MD45>
2023-01-29 13:36:03 +0530

    Add new

--- Batch5/src/com/add/Add.java
+++ Batch5/src/com/add/Add.java
@@ -3,7 +3,8 @@ package com.add;
 public class Add {
 	public static void main(String[] args) {
 		int add,a=5,b=89;
-		add=a+b;
+		int c=5;
+		add=a+b+c;
 		System.out.println("the sum is"+add);
 	}
 	
