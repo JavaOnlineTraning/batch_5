d6f9b0f3a643cd21019c234e5a4d2128a1323dd6
Hemraj <Hemraj@DESKTOP-U0HKUMH>
2023-01-30 17:19:54 +0530

    Swapping Program

--- Batch5/src/com/basic/Swapping.java
+++ Batch5/src/com/basic/Swapping.java
@@ -1,10 +1,20 @@
 package com.basic;
+import java.util.Scanner;
 
 public class Swapping {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a,b,c;
+		Scanner Sc = new Scanner (System.in);
+    System.out.println("Enter The Number");
+    a=Sc.nextInt();
+    b=Sc.nextInt();
+    System.out.println("before swapping number :"+a+" "+b);
+    c=a;
+    a=b;
+    b=c;
+    System.out.println("after swapping number :"+a+" "+b);
+    System.out.println();
 	}
 
 }
