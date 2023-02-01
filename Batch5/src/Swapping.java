44f95f21ce41f86724b6be90d419c3577637ec1b
abhijeet thakur <abhijeet thakur@LAPTOP-12KC92Q4>
2023-02-01 08:44:50 +0530

    swapping

---
+++ Batch5/src/Swapping.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    int x,y,z;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of x and y");
+         
+        x=sc.nextInt();
+        y=sc.nextInt();
+        
+        System.out.println("before swapping number : "+x+" "+y);
+        
+        //Swapping
+       /* z = x;
+        x = y;
+        y = z;*/
+        
+        x = x+y;   //x = 3,  y =5  y= 3+5 = 8
+        y = x-y;    // x = 8 ,     y=5 = 3
+        x = x-y;    // y = 3 ,     x=8-3 = 5 
+                    // x = 5
+        
+        System.out.println("After Swapping : "+x+" "+y);
+        System.out.println();
+	
+	}
+
+}
