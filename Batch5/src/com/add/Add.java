bc196a4cbe52c9f96136609c0b56e262ca80a4b4
Rahul <Rahul@LAPTOP-RASFCALN>
2023-02-01 23:04:44 +0530

    Added new variable

--- Batch5/src/com/add/Add.java
+++ Batch5/src/com/add/Add.java
@@ -2,10 +2,12 @@ package com.add;
 
 public class Add {
 	public static void main(String[] args) {
-		int add,a=5,b=89;
-		add=a+b;
+		int add,a=5,b=89,c=100;
+		add=a+b+c;
 		System.out.println("the sum is"+add);
 		System.out.println("the sum is"+add);
+		
+		
 		
 	}
 	
