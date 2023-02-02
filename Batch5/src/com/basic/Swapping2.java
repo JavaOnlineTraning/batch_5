e68568a0925c2f25743947d7f2def55b8e4534f4
Vaishnaviparaskar <paraskarv4@gmail.com>
2023-02-02 19:33:34 +0530

    Swapping Program

--- Batch5/src/com/basic/Swapping2.java
+++ Batch5/src/com/basic/Swapping2.java
@@ -3,7 +3,22 @@ package com.basic;
 public class Swapping2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x,y;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter value for X and Y");
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		x=x+y;  // using 2 variables
+		y=x-y;
+		x=x-y;
+		
+		System.out.println("After Swapping X and Y");
+		System.out.println(x+" "+y);
+		
+	
 
 	}
 
