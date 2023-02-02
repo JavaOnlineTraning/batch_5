5968a1b2f2d0322bdf31c4a9b43b3e7318124ccb
akshay <akshay@DESKTOP-O210Q6V>
2023-02-02 16:14:13 +0530

    update

--- CrudApi2/src/main/java/com/spring/CrudOperation/CrudApiApplication.java
+++ CrudApi2/src/main/java/com/spring/CrudOperation/CrudApiApplication.java
@@ -8,6 +8,7 @@ public class CrudApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApiApplication.class, args);
+		System.out.println("spring boot Sarted...");
 	}
 
 }
