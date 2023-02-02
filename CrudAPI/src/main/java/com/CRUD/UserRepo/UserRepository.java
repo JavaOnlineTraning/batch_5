0d56ff134dceb42a89d0c6d63819a697b3f978a7
akshay <akshay@DESKTOP-O210Q6V>
2023-02-02 14:53:22 +0530

    spring boot project

---
+++ CrudAPI/src/main/java/com/CRUD/UserRepo/UserRepository.java
@@ -0,0 +1,8 @@
+package com.CRUD.UserRepo;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+
+}
---
+++ CrudAPI/src/main/java/com/CRUD/service/ServiceLayer.java
@@ -0,0 +1,5 @@
+package com.CRUD.service;
+
+public interface ServiceLayer {
+
+}
