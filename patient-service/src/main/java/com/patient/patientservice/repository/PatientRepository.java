d59119ba2607b1d946824cb98dda13bcbe6debad
admin1 <admin1@BYOD-598793>
2023-02-03 22:16:43 +0530

    iterface

--- patient-service/src/main/java/com/patient/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/patient/patientservice/repository/PatientRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.patient.patientservice.entities.PatientEntity;
 
 public interface PatientRepository extends JpaRepository<PatientEntity, String> {
+	//from java 1.8 static and default methods are allowed in Interface, Why?
+	//before java 1.8 only abstract methods are allowed in Interface
 
 }
