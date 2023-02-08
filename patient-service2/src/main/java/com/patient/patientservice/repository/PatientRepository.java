4a46ff5067995648ecb43cbec8b3bea9880859c2
akshay <akshay@DESKTOP-O210Q6V>
2023-02-08 09:31:45 +0530

    patient service project

---
+++ patient-service2/src/main/java/com/patient/patientservice/repository/PatientRepository.java
@@ -0,0 +1,15 @@
+package com.patient.patientservice.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.patient.patientservice.entities.PatientEntity;
+
+public interface PatientRepository extends JpaRepository<PatientEntity, Integer> {
+	// from java 1.8 static and default methods are allowed in Interface, Why?
+	// before java 1.8 only abstract methods are allowed in Interface
+
+	List<PatientEntity> findAllByPatientName(String patientName);
+	
+}
